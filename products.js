4d8a6803f356d88a37d7213d48dff2ec2f573549
ahmadxyzjhc <shaheenazehgeer7@gmail.com>
2026-06-28 13:03:05 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,29 @@
+const products = [
+{
+    name: "Face Wash",
+    image: "facewash.jpg",
+    category: "Skin Care",
+    description: "Premium quality face wash for healthy skin."
+},
 
+{
+    name: "Lipstick",
+    image: "https://via.placeholder.com/250x250?text=Lipstick",
+    category: "Makeup",
+    description: "Long-lasting lipstick."
+},
+
+{
+    name: "Perfume",
+    image: "https://via.placeholder.com/250x250?text=Perfume",
+    category: "Fragrance",
+    description: "Long-lasting fragrance."
+},
+
+{
+    name: "Socks",
+    image: "https://via.placeholder.com/250x250?text=Socks",
+    category: "Hosiery",
+    description: "Comfortable cotton socks."
+}
+];
